ff844ac9b32015b92786d0587862ef79d2f2fc27
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-04-27 22:45:50 +0100

    addded server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const PORT = 5000;
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`server running ${PORT}`);
+});
