71172d641b9a340c43180a3a11ab1d9ed164f9ac
Chiheb <chihababiza9@gmail.com>
2025-04-28 12:08:38 +0100

    .

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(MONGO_URI,);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
