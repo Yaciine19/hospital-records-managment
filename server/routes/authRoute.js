43b93de4c455190d2f320c1aea17e58c2f5185a8
yacine <yacineddd32@gmail.com>
2025-04-28 13:08:11 +0100

    Add: middleware auth (protect)

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/authController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const authRoute = Router();
 
 // Login
 authRoute.post("/login", loginUser);
 
-// Register new user
-authRoute.post("/register", registerUser);
+// Register new user (Admin only)
+authRoute.post("/register", protect, registerUser);
 
 export default authRoute;
