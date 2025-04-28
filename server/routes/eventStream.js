0f46464e4c88878957f54084ff28b38ca867c066
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-04-28 13:26:28 +0100

    event stream refactor

--- server/routes/eventStream.js
+++ server/routes/eventStream.js
@@ -6,57 +6,73 @@ const deathQueue = [];
 const birthQueue = [];
 let messageCounter = 0;
 
+const deathClients = new Set();
+const birthClients = new Set();
+
 export function emitBirth(record) {
   birthQueue.push(record);
+  broadcastToClients(birthClients, record);
 }
 
 export function emitDeath(record) {
   deathQueue.push(record);
+  broadcastToClients(deathClients, record);
 }
 
-const prepareHeaders = (res, interval) => {
+function broadcastToClients(clients, record) {
+  if (clients.size > 0) {
+    messageCounter += 1;
+    const data = JSON.stringify(record);
+    const message = `id: ${messageCounter}\ndata: ${data}\n\n`;
+    
+    clients.forEach(client => {
+      client.write(message);
+    });
+    
+    console.log(`Broadcast to ${clients.size} clients:`, data);
+  }
+}
+
+const prepareHeaders = (res) => {
   res.set({
     "Cache-Control": "no-cache",
     "Content-Type": "text/event-stream",
     Connection: "keep-alive",
   });
-
   res.flushHeaders();
-
-  res.on("close", function() {
-    clearInterval(interval);
-    console.log("Client stopped listening");
-  });
 };
 
-const sendRecord = (res, record) => {
-  messageCounter += 1;
-
-  let data = JSON.stringify(record);
-  console.log(data);
-
-  res.write("id: " + messageCounter + "\n");
-  res.write("data: " + JSON.stringify(record) + "\n\n");
-};
-
-router.get("/eventsDeath", async function(req, res) {
+router.get("/eventsDeath", function(req, res) {
   prepareHeaders(res);
-  while (true) {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    if (deathQueue.length > 0) {
-      sendRecord(res, birthQueue.pop());
-    }
+  
+  deathClients.add(res);
+  
+  if (deathQueue.length > 0) {
+    const record = deathQueue.shift();
+    messageCounter += 1;
+    res.write(`id: ${messageCounter}\ndata: ${JSON.stringify(record)}\n\n`);
   }
+  
+  res.on("close", function() {
+    deathClients.delete(res);
+    console.log("Death event client disconnected, remaining clients:", deathClients.size);
+  });
 });
 
-router.get("/eventsBirth", async function(req, res) {
+router.get("/eventsBirth", function(req, res) {
   prepareHeaders(res);
-  while (true) {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    if (birthQueue.length > 0) {
-      sendRecord(res, birthQueue.pop());
-    }
+
+  birthClients.add(res);
+  if (birthQueue.length > 0) {
+    const record = birthQueue.shift();
+    messageCounter += 1;
+    res.write(`id: ${messageCounter}\ndata: ${JSON.stringify(record)}\n\n`);
   }
+  
+  res.on("close", function() {
+    birthClients.delete(res);
+    console.log("Birth event client disconnected, remaining clients:", birthClients.size);
+  });
 });
 
 export default router;
